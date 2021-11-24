f1355102422d58701aded3527b5bdf383e3a691d
Piotr Ludynia <pietrekpl18@gmail.com>
2021-11-24 21:59:03 +0100

    add dependency to pom, add class ExcelWriter

---
+++ src/main/java/ExcelWriter.java
@@ -0,0 +1,14 @@
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelWriter {
+
+
+XSSFWorkbook workbook = new XSSFWorkbook();
+
+XSSFSheet countOccurencesSheet = workbook.createSheet("Count occurrences");
+
+
+// to complete the rest of a class
+
+}
